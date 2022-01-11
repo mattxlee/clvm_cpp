93c5d027a6fd1ad2691f3af916ea87d76ed0f01c
Matthew Lee <mattxlee@gmail.com>
2022-01-11 23:58:11 +0800

    Add `Program` to parse bytes into sexp.

--- include/program.h
+++ include/program.h
@@ -1,11 +1,86 @@
 #ifndef CHIA_PROGRAM_H
 #define CHIA_PROGRAM_H
 
+#include <functional>
+#include <memory>
+#include <stack>
+
 #include "types.h"
 
 namespace chia {
 
 static std::string_view DEFAULT_HIDDEN_PUZZLE = "ff0980";
+
+enum class NodeType : int { Atom, Pair };
+
+class CLVMObject;
+using CLVMObjectPtr = std::shared_ptr<CLVMObject>;
+
+class CLVMObject {
+ public:
+  explicit CLVMObject(NodeType type);
+
+  virtual ~CLVMObject() {}
+
+  NodeType GetNodeType() const { return type_; }
+
+ private:
+  NodeType type_;
+};
+
+class CLVMObject_Atom : public CLVMObject {
+ public:
+  explicit CLVMObject_Atom(Bytes bytes);
+
+  Bytes GetBytes() const;
+
+ private:
+  Bytes bytes_;
+};
+
+class CLVMObject_Pair : public CLVMObject {
+ public:
+  CLVMObject_Pair(CLVMObjectPtr first, CLVMObjectPtr second);
+
+  CLVMObjectPtr GetFirstNode() const;
+
+  CLVMObjectPtr GetSecondNode() const;
+
+ private:
+  CLVMObjectPtr first_;
+  CLVMObjectPtr second_;
+};
+
+using StreamReadFunc = std::function<Bytes(int size)>;
+
+template <typename T>
+class Stack {
+ public:
+  void Push(T op) { stack_.push(std::move(op)); }
+
+  T Pop() {
+    T res = stack_.top();
+    stack_.pop();
+    return res;
+  }
+
+  bool IsEmpty() const { return stack_.empty(); }
+
+ private:
+  std::stack<T> stack_;
+};
+
+using ValStack = Stack<CLVMObjectPtr>;
+
+class OpStack;
+
+using Op = std::function<void(OpStack&, ValStack&, StreamReadFunc)>;
+
+class OpStack : public Stack<Op> {};
+
+using ReadStreamFunc = std::function<Bytes(int size)>;
+
+CLVMObjectPtr SExpFromStream(ReadStreamFunc&& f);
 
 class Result {
  public:
--- src/program.cpp
+++ src/program.cpp
@@ -1,6 +1,128 @@
 #include "program.h"
 
+#include "utils.h"
+
 namespace chia {
+
+uint8_t const MAX_SINGLE_BYTE = 0x7F;
+uint8_t const CONS_BOX_MARKER = 0xFF;
+
+/**
+ * =============================================================================
+ * CLVMObject
+ * =============================================================================
+ */
+
+CLVMObject::CLVMObject(NodeType type) {}
+
+CLVMObject_Atom::CLVMObject_Atom(Bytes bytes)
+    : CLVMObject(NodeType::Atom), bytes_(std::move(bytes)) {}
+
+CLVMObject_Pair::CLVMObject_Pair(CLVMObjectPtr first, CLVMObjectPtr second)
+    : CLVMObject(NodeType::Pair), first_(first), second_(second) {}
+
+/**
+ * =============================================================================
+ * Op: SExp
+ * =============================================================================
+ */
+
+CLVMObjectPtr ToSExp(Bytes bytes) {
+  return CLVMObjectPtr(new CLVMObject_Atom(std::move(bytes)));
+}
+
+CLVMObjectPtr ToSExp(CLVMObjectPtr first, CLVMObjectPtr second) {
+  return CLVMObjectPtr(new CLVMObject_Pair(first, second));
+}
+
+template <typename T>
+T IntFromBytesBE(Bytes const& bytes) {
+  Bytes be;
+  std::copy(std::rbegin(bytes), std::rend(bytes), std::back_inserter(be));
+  int padding_bytes{0};
+  if (bytes.size() < sizeof(T)) {
+    padding_bytes = sizeof(T) - bytes.size();
+  }
+  for (int i = 0; i < padding_bytes; ++i) {
+    be.push_back(0);
+  }
+  T res;
+  memcpy(&res, be.data(), sizeof(T));
+  return res;
+}
+
+CLVMObjectPtr AtomFromStream(StreamReadFunc f, uint8_t b) {
+  if (b == 0x80) {
+    return ToSExp(Bytes());
+  }
+  if (b <= MAX_SINGLE_BYTE) {
+    Bytes bytes{b};
+    return ToSExp(std::move(bytes));
+  }
+  int bit_count{0};
+  int bit_mask{0x80};
+  while (b & bit_mask) {
+    bit_count += 1;
+    b &= 0xff ^ bit_mask;
+    bit_mask >>= 1;
+  }
+  Bytes size_blob{b};
+  if (bit_count > 1) {
+    Bytes b = f(bit_count - 1);
+    if (b.size() != bit_count - 1) {
+      throw std::runtime_error("bad encoding");
+    }
+    size_blob = utils::ConnectContainers(size_blob, b);
+  }
+  auto size = IntFromBytesBE<uint64_t>(size_blob);
+  if (size >= 0x400000000) {
+    throw std::runtime_error("blob too large");
+  }
+  Bytes blob = f(size);
+  if (blob.size() != size) {
+    throw std::runtime_error("bad encoding");
+  }
+  return ToSExp(blob);
+}
+
+void OpCons(OpStack& op_stack, ValStack& val_stack, StreamReadFunc f) {
+  auto right = val_stack.Pop();
+  auto left = val_stack.Pop();
+  val_stack.Push(ToSExp(left, right));
+}
+
+void OpReadSExp(OpStack& op_stack, ValStack& val_stack, StreamReadFunc f) {
+  Bytes blob = f(1);
+  if (blob.empty()) {
+    throw std::runtime_error("bad encoding");
+  }
+  uint8_t b = blob[0];
+  if (b == CONS_BOX_MARKER) {
+    op_stack.Push(OpCons);
+    op_stack.Push(OpReadSExp);
+    op_stack.Push(OpReadSExp);
+    return;
+  }
+  val_stack.Push(AtomFromStream(f, b));
+}
+
+CLVMObjectPtr SExpFromStream(ReadStreamFunc f) {
+  OpStack op_stack;
+  op_stack.Push(OpReadSExp);
+  ValStack val_stack;
+
+  while (!op_stack.IsEmpty()) {
+    auto func = op_stack.Pop();
+    func(op_stack, val_stack, f);
+  }
+  return val_stack.Pop();
+}
+
+/**
+ * =============================================================================
+ * Program
+ * =============================================================================
+ */
 
 Program Program::ImportFromBytes(Bytes const& bytes) { return Program(); }
 
