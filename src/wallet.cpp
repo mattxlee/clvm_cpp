121d40cff2b71f4aa7c5e9640abf389d3bf10e64
Matthew Lee <mattxlee@gmail.com>
2022-01-11 18:15:02 +0800

    Complete method `GetAddress` of Wallet.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1,5 +1,10 @@
 #include "wallet.h"
 
+#include "bech32.h"
+#include "clvm.h"
+#include "program.h"
+#include "utils.h"
+
 namespace chia {
 namespace wallet {
 
@@ -15,7 +20,14 @@ Wallet::Wallet(std::string_view words, std::string_view passphrase)
 Address Wallet::GetAddress(int index) const {
   Key key = GetKey(index);
   PublicKey pub_key = key.GetPublicKey();
-  // TODO Convert pub-key to address
+  Bytes32 puzzle_hash = clvm::PuzzleForPk(
+      pub_key,
+      Program::ImportFromBytes(utils::BytesFromHex(DEFAULT_HIDDEN_PUZZLE))
+          .GetTreeHash());
+  std::vector<int> puzzle_hash_ivec;
+  std::copy(std::begin(puzzle_hash), std::end(puzzle_hash),
+            std::back_inserter(puzzle_hash_ivec));
+  return bech32::Encode("xch", bech32::ConvertBits(puzzle_hash_ivec, 8, 5));
 }
 
 Key Wallet::GetKey(uint32_t index) const {
