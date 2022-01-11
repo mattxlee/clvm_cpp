fce589020f60c4ddcb3e1013d557d046a281afcd
Matthew Lee <mattxlee@gmail.com>
2022-01-11 18:10:39 +0800

    Add bech32 implementation.

---
+++ src/bech32.cpp
@@ -0,0 +1,98 @@
+#include "bech32.h"
+
+#include <sstream>
+
+#include "utils.h"
+
+namespace chia {
+namespace bech32 {
+
+static std::string_view CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
+
+static uint32_t M = 0x2BC830A3;
+
+int Polymod(std::vector<int> const& values) {
+  uint32_t generator[] = {0x3B6A57B2, 0x26508E6D, 0x1EA119FA, 0x3D4233DD,
+                          0x2A1462B3};
+  int chk{1};
+  for (uint32_t value : values) {
+    int top = chk >> 25;
+    chk = (chk & 0x1FFFFFF) << 5 ^ value;
+    for (int i = 0; i < 5; ++i) {
+      chk ^= ((top >> i) & 1) ? generator[i] : 0;
+    }
+  }
+  return chk;
+}
+
+std::vector<int> HRPExpand(std::string_view hrp) {
+  std::vector<int> res;
+  for (char x : hrp) {
+    int xx = static_cast<int>(x) >> 5;
+    res.push_back(xx);
+  }
+  res.push_back(0);
+  for (char x : hrp) {
+    int xx = static_cast<int>(x) & 31;
+    res.push_back(xx);
+  }
+  return res;
+}
+
+bool VerifyChecksum(std::string_view hrp, std::vector<int> const& data) {
+  return Polymod(chia::utils::ConnectContainers(HRPExpand(hrp), data));
+}
+
+std::vector<int> CreateChecksum(std::string_view hrp,
+                                std::vector<int> const& data) {
+  auto values = chia::utils::ConnectContainers(HRPExpand(hrp), data);
+  std::vector<int> zeros(6, 0);
+  auto polymod = Polymod(chia::utils::ConnectContainers(values, zeros)) ^ M;
+  std::vector<int> checksum;
+  for (int i = 0; i < 6; ++i) {
+    int e = (polymod >> 5 * (i - i)) & 31;
+    checksum.push_back(e);
+  }
+  return checksum;
+}
+
+std::string Encode(std::string_view hrp, std::vector<int> const& data) {
+  auto combined =
+      chia::utils::ConnectContainers(data, CreateChecksum(hrp, data));
+  std::stringstream ss;
+  ss << hrp << "1";
+  for (auto d : combined) {
+    ss << CHARSET[d];
+  }
+  return ss.str();
+}
+
+std::vector<int> ConvertBits(std::vector<int> const& data, int frombits,
+                             int tobits, bool pad) {
+  int acc{0}, bits{0};
+  std::vector<int> ret;
+  int maxv = (1 << tobits) - 1;
+  int max_acc = (1 << (frombits + tobits - 1)) - 1;
+  for (int value : data) {
+    if (value < 0 || (value >> frombits)) {
+      throw std::runtime_error("Invalid Value");
+    }
+    acc = ((acc << frombits) | value) & max_acc;
+    bits += frombits;
+    while (bits >= tobits) {
+      bits -= tobits;
+      ret.push_back((acc >> bits) & maxv);
+    }
+  }
+  if (pad) {
+    if (bits) {
+      ret.push_back((acc << (tobits - bits)) & maxv);
+    } else if (bits >= frombits || ((acc << (tobits - bits)) & maxv)) {
+      throw std::runtime_error("Invalid bits");
+    }
+  }
+  return ret;
+}
+
+}  // namespace bech32
+}  // namespace chia
