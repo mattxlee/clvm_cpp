cf47d7795fb047404cc3b6d549e6477d596fe678
Matthew Lee <mattxlee@gmail.com>
2022-01-14 18:00:41 +0800

    Improve test cases.

--- tests/test_sha256_treehash.cpp
+++ tests/test_sha256_treehash.cpp
@@ -6,27 +6,51 @@
 #include "types.h"
 #include "utils.h"
 
+TEST(Utilities, ByteToBytes) {
+  auto bytes = chia::utils::ByteToBytes('\1');
+  EXPECT_EQ(bytes[0], '\1');
+}
+
+TEST(Utilities, Bytes) {
+  EXPECT_EQ(chia::utils::Byte4bToHexChar(10), 'a');
+  EXPECT_EQ(chia::utils::HexCharToByte4b('b'), 11);
+
+  chia::Bytes bytes(2);
+  bytes[0] = 0xab;
+  bytes[1] = 0xef;
+  EXPECT_EQ(chia::utils::BytesToHex(bytes), "abef");
+  EXPECT_EQ(chia::utils::BytesFromHex("abef"), bytes);
+
+  chia::Bytes empty;
+  EXPECT_TRUE(chia::utils::ConnectBuffers(empty, empty).empty());
+
+  EXPECT_EQ(chia::utils::ConnectBuffers(bytes, bytes),
+            chia::utils::BytesFromHex("abefabef"));
+
+  EXPECT_EQ(chia::utils::ConnectBuffers(empty, bytes),
+            chia::utils::BytesFromHex("abef"));
+
+  EXPECT_EQ(chia::utils::ConnectBuffers(bytes, empty),
+            chia::utils::BytesFromHex("abef"));
+}
+
+std::string_view const s0 = "../clvm/calculate_synthetic_public_key.clvm.hex";
+std::string_view const s0_treehash =
+    "../clvm/calculate_synthetic_public_key.clvm.hex.sha256tree";
+
 std::string_view const s1 =
     "../clvm/p2_delegated_puzzle_or_hidden_puzzle.clvm.hex";
-
 std::string_view const s1_treehash =
     "../clvm/p2_delegated_puzzle_or_hidden_puzzle.clvm.hex.sha256tree";
 
-chia::Bytes LoadCLVM(std::string_view file_path) {
-  std::ifstream in(file_path, std::ios::binary);
-  if (!in.is_open()) {
-    throw std::runtime_error("cannot open file to read");
-  }
-  in.seekg(0, std::ios::end);
-  auto p = in.tellg();
-  in.seekg(0, std::ios::beg);
-  chia::Bytes res(p);
-  in.read(reinterpret_cast<char*>(res.data()), p);
-  assert(in.gcount() == p);
-  return res;
+TEST(CLVM_SHA256_treehash, LoadAndVerify_s0) {
+  auto prog = chia::Program::LoadFromFile(s0);
+  auto treehash_bytes =
+      chia::utils::BytesFromHex(chia::utils::LoadHexFromFile(s0_treehash));
+  EXPECT_EQ(chia::utils::bytes_cast<32>(prog.GetTreeHash()), treehash_bytes);
 }
 
-TEST(CLVM_SHA256_treehash, LoadAndVerify) {
+TEST(CLVM_SHA256_treehash, LoadAndVerify_s1) {
   auto prog = chia::Program::LoadFromFile(s1);
   auto treehash_bytes =
       chia::utils::BytesFromHex(chia::utils::LoadHexFromFile(s1_treehash));
